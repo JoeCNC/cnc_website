766d7397d88b9e4a6358bdb93c65c8afae61c5be
JoeCNC <benoitclement.data@gmail.com>
2025-07-11 16:06:35 +0200

    Initial commit

---
+++ vocs.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vocs'
+
+export default defineConfig({
+  title: 'Portfolio',
+  topNav: [ 
+    { text: 'CV', link: '/CV', match: '/docs' }, 
+    { text: 'Blog', link: '/blog' }
+  ],
+  sidebar: [
+    {
+      text: 'Projets',
+      link: '/projects',
+    },
+    {
+      text: 'Projet n°1',
+      link: '/project1',
+    },
+  ],
+})
